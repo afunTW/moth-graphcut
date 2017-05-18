63004faab42e6397915796e3829ac186efd2eb65
afunTW <endlessproof@gmail.com>
2017-05-18 16:54:39 +0800

    modified: saved smoothing line

--- src/graphic.py
+++ src/graphic.py
@@ -549,15 +549,15 @@ class GraphCut(object):
         for side, track in self.__tracking_label.items():
             if side in [self.ON_LEFT, self.ON_RIGHT] and track:
                 if len(track) > 10:
-                    track = self.get_smooth_line(track)
+                    self.__tracking_label[side] = self.get_smooth_line(track)
                 for i, ptx in enumerate(track):
                     if i == 0: continue
                     cv2.line(self.__panel_img, track[i-1], ptx, self.BLACK, 2)
 
             elif side == 'eliminate':
-                for block in track:
+                for n, block in enumerate(track):
                     if len(block) > 10:
-                        block = self.get_smooth_line(block)
+                        self.__tracking_label[side][n] = self.get_smooth_line(block)
                     for i, ptx in enumerate(block):
                         if i == 0: continue
                         pt1 = block[i-1]
