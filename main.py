508999b9cc597be28ff330e76548cb9c53684798
afunTW <endlessproof@gmail.com>
2017-05-23 16:44:10 +0800

    add: argparse -a, -all command set --all will process all image, otherwise, process 'pause' first then 'none'

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import sys
 import cv2
 import json
 import logging
+import argparse
 import numpy as np
 
 from hashlib import sha1
 from src import detector
 from src import graphic
 
+
+def argparser():
+    parser = argparse.ArgumentParser(description='interactive graph cut for moth image')
+    parser.add_argument('-a', '--all', help='process all image',
+        action='store_true', default=False)
+    return parser
 
 def filter_rects(rects, ignore_rect):
     result = []
@@ -29,23 +36,7 @@ def is_bottom_right(rects):
             continue
     return result
 
-def all_to_list(f):
-    assert isinstance(f, dict)
-
-    for k, v in f.items():
-        if isinstance(v, dict):
-            f.update({k: all_to_list(v)})
-        elif isinstance(v, np.ndarray):
-            f.update({k: v.tolist()})
-        elif isinstance(v, tuple):
-            v = tuple(i.tolist() if isinstance(i, np.ndarray) else i for i in v)
-            f.update({k: v})
-        else:
-            print('Not support {} in the type of {}'.format(k, type(v)))
-
-    return f
-
-def main(filename, template=None, last_status=None):
+def graph_cut(filename, template=None, last_status=None):
 
     # filter
     orig_image = None
@@ -74,7 +65,7 @@ def main(filename, template=None, last_status=None):
                 orig_image[y-5:y+h+5, x-5:x+w+5, :] = 255
 
     # graphic
-    gb = graphic.GraphCut(filename, orig_image=orig_image)
+    gc = graphic.GraphCut(filename, orig_image=orig_image)
     logging.info(' Process graph cut')
 
     if last_status:
@@ -82,17 +73,105 @@ def main(filename, template=None, last_status=None):
 
         if last_status['mirror_line']:
             pt1 , pt2 = last_status['mirror_line']
-            gb.mirror_line = (tuple(pt1), tuple(pt2))
+            gc.mirror_line = (tuple(pt1), tuple(pt2))
 
         if last_status['mirror_shift']:
-            gb.mirror_shift = last_status['mirror_shift']
+            gc.mirror_shift = last_status['mirror_shift']
 
         if any([v for v in last_status['tracking_label']]):
-            gb.tracking_label = last_status['tracking_label']
+            gc.tracking_label = last_status['tracking_label']
 
-    gb.run()
+    gc.run()
 
-    return gb
+    return gc
+
+def all_to_list(f):
+    assert isinstance(f, dict)
+
+    for k, v in f.items():
+        if isinstance(v, dict):
+            f.update({k: all_to_list(v)})
+        elif isinstance(v, np.ndarray):
+            f.update({k: v.tolist()})
+        elif isinstance(v, tuple):
+            v = tuple(i.tolist() if isinstance(i, np.ndarray) else i for i in v)
+            f.update({k: v})
+        else:
+            print('Not support {} in the type of {}'.format(k, type(v)))
+
+    return f
+
+def saved_metadata(gc, saved_file):
+    data = {
+        'name': gc.filename,
+        'state': gc.STATE,
+        'mirror_line': gc.mirror_line,
+        'mirror_shift': gc.mirror_shift,
+        'tracking_label': gc.tracking_label,
+        'components_color': all_to_list(gc.components_color.copy()),
+        'components_contour': all_to_list(gc.components_contour.copy())
+    }
+
+    with open(saved_file, 'w+') as f:
+        json.dump(data, f)
+
+    if gc.STATE == 'pause':
+        exit()
+
+def main():
+    try:
+        template_path = 'image/10mm.png'
+        metadata_path = 'metadata/'
+        metadata_path = os.path.abspath(metadata_path)
+        metadata_map = os.path.join(metadata_path, 'map.json')
+        moths_path = os.path.abspath('image/sample')
+        moths = [os.path.join(moths_path, moth) for moth in os.listdir(moths_path)]
+        parser = argparser()
+
+        # checked file
+        if not os.path.exists(metadata_path):
+            os.makedirs(metadata_path)
+        if not os.path.exists(metadata_map):
+            with open(metadata_map, 'w') as f:
+                json.dump({}, f)
+
+        with open(metadata_map, 'r') as f:
+            exist_data = json.load(f)
+
+        # core
+        for i, moth in enumerate(moths):
+            logging.info('({}/{}) Process {}'.format(
+                i+1, len(moths), moth.split('/')[-1]))
+
+            key = sha1(moth.encode('utf-8')).hexdigest()
+            key_json = ''.join([key, '.json'])
+            key_json = os.path.join(metadata_path, key_json)
+            result = None
+
+            if parser.parse_args().all:
+                if key in exist_data.keys() and os.path.exists(key_json):
+                    with open(key_json, 'r') as f:
+                        last_status = json.load(f)
+                        result = graph_cut(moth, template_path, last_status)
+            else:
+                if key in exist_data.keys() and os.path.exists(key_json):
+                    if exist_data[key]['state'] == 'done': continue
+                    elif exist_data[key]['state'] == 'pause':
+                        with open(key_json, 'r') as f:
+                            last_status = json.load(f)
+                            result = graph_cut(moth, template_path, last_status)
+
+            if result is None:
+                result = graph_cut(moth, template_path)
+
+            with open(metadata_map, 'w') as f:
+                exist_data.update({key: {'file': moth, 'state': result.STATE}})
+                json.dump(exist_data, f, indent=4)
+
+            saved_metadata(result, key_json)
+
+    except Exception as e:
+        logging.exception(e)
 
 if __name__ == '__main__':
 
@@ -103,56 +182,4 @@ if __name__ == '__main__':
         stream=sys.stdout
         )
 
-    try:
-        template_path = 'image/10mm.png'
-        metadata_path = 'metadata/'
-        metadata_path = os.path.abspath(metadata_path)
-        metadata_map = os.path.join(metadata_path, 'map.json')
-        moths_path = os.path.abspath('image/sample')
-        moths = [os.path.join(moths_path, moth) for moth in os.listdir(moths_path)]
-
-        if not os.path.exists(metadata_path):
-            os.makedirs(metadata_path)
-
-        if not os.path.exists(metadata_map):
-            with open(metadata_map, 'w') as f:
-                json.dump({}, f)
-
-        with open(metadata_map, 'r') as f:
-            exist_data = json.load(f)
-
-        for i, moth in enumerate(moths):
-            logging.info('({}/{}) Process {}'.format(
-                i+1, len(moths), moth.split('/')[-1]))
-
-            key = sha1(moth.encode('utf-8')).hexdigest()
-            key_json = ''.join([key, '.json'])
-            key_json = os.path.join(metadata_path, key_json)
-
-            if key in exist_data.keys() and os.path.exists(key_json):
-                with open(key_json, 'r') as f:
-                    last_status = json.load(f)
-                result = main(moth, template=template_path, last_status=last_status)
-            else:
-                result = main(moth, template=template_path)
-                with open(metadata_map, 'w') as f:
-                    exist_data.update({key: moth})
-                    json.dump(exist_data, f, indent=4)
-
-            data = {
-                'name': moth,
-                'state': result.STATE,
-                'mirror_line': result.mirror_line,
-                'mirror_shift': result.mirror_shift,
-                'tracking_label': result.tracking_label,
-                'components_color': all_to_list(result.components_color.copy()),
-                'components_contour': all_to_list(result.components_contour.copy())
-            }
-
-            with open(key_json, 'w+') as f:
-                json.dump(data, f)
-
-            # break
-
-    except Exception as e:
-        logging.exception(e)
+    main()
