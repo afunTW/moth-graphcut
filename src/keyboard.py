14b78d70cf67013745f14433db86bbf6bb1d0df6
afunTW <endlessproof@gmail.com>
2017-06-13 12:14:13 +0800

    add: modularized keyboard handler

---
+++ src/keyboard.py
@@ -0,0 +1,15 @@
+import os
+
+class KeyHandler(object):
+    def __init__(self):
+        super().__init__()
+        if os.name == 'posix':
+            self.KEY_LEFT = 81
+            self.KEY_UP = 82
+            self.KEY_RIGHT = 83
+            self.KEY_DOWN = 84
+        elif os.name == 'nt':
+            self.KEY_LEFT = 2424832
+            self.KEY_UP = 2490368
+            self.KEY_RIGHT = 2555904
+            self.KEY_DOWN = 2621440
