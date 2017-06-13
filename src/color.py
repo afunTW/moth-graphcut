e63897e43a5851ae50f7b8c55163f65194c6ffce
afunTW <endlessproof@gmail.com>
2017-06-13 12:13:21 +0800

    add: modularized color

---
+++ src/color.py
@@ -0,0 +1,8 @@
+class ImageColor(object):
+    def __init__(self):
+        super().__init__()
+        self.BLUE = [255,0,0]
+        self.RED = [0,0,255]
+        self.GREEN = [0,255,0]
+        self.BLACK = [0,0,0]
+        self.WHITE = [255,255,255]
