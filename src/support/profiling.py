a6079a6a6f0d42d6182ee860ca9454034157ad54
afunTW <endlessproof@gmail.com>
2017-08-20 13:41:49 +0800

    add: profiling decorator

---
+++ src/support/profiling.py
@@ -0,0 +1,38 @@
+"""
+Support function for python profilling
+"""
+
+import time
+import logging
+from functools import wraps
+
+
+LOGGER = logging.getLogger(__name__)
+
+def func_profiling(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        time_spent = time.time() - start_time
+        fullname = '{}.{}'.format(func.__module__, func.__name__)
+        LOGGER.info('{}[args={}, kwargs={}] completed in {}'.format(
+            fullname, args, kwargs, time_spent
+        ))
+        return result
+    return wrapped
+
+@func_profiling
+def test_func_profiling(sec):
+    time.sleep(sec)
+
+if __name__ == '__main__':
+    """testing"""
+    import sys
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(filename)12s:L%(lineno)3s [%(levelname)8s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        stream=sys.stdout
+        )
+    test_func_profiling(1)
