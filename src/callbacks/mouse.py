1f5fc9809a1e9a864eca367ad3d9468aa3e41a9a
afunTW <endlessproof@gmail.com>
2017-08-23 17:03:13 +0800

    add: draw line by cv2

--- src/callbacks/mouse.py
+++ src/callbacks/mouse.py
@@ -5,13 +5,28 @@ import logging
 import sys
 sys.path.append('../')
 
+import numpy as np
+
 from support.profiling import func_profiling
+from view.tkconvert import TkConverter
 
 LOGGER = logging.getLogger(__name__)
 
 @func_profiling
-def draw_circle(e, w, color='#000000'):
-    """default draw with balck color"""
-    x1, y1 = (e.x-1), (e.y-1)
-    x2, y2 = (e.x+1), (e.y+1)
-    w.create_oval(x1, y1, x2, y2, fill=color)
+def draw_symmetric_line_by_cv2(e, widget=None, image=None, color='255'):
+    if widget is None:
+        LOGGER.error('No widget has been selected.')
+    elif image is None:
+        LOGGER.error('No image has been selected.')
+        return None
+    elif not isinstance(image, np.ndarray):
+        LOGGER.error('Image {} is not in np.ndarray format.'.format(type(image)))
+        return None
+    else:
+        # draw the line by OpenCV
+        h, w, channels = image.shape
+        cv2.line(image, (e.x, 0), (e.x, h), color, 2)
+
+        # update the image to widget
+        photo = TkConverter.ndarray_to_photo(image)
+        widget.configure(image=photo)
