56855d89e03dfae17acfc8134451a6770ab642a9
afunTW <endlessproof@gmail.com>
2017-09-15 15:58:27 +0800

    add: switch to next/previous N image

--- src/actions/keyboard.py
+++ src/actions/keyboard.py
@@ -16,14 +16,17 @@ class MothKeyboardHandler(MothViewerTemplate):
 
     # press UP and switch to previous image in image queue
     def switch_to_previous_image(self, event=None, step=1):
+        LOGGER.info('step = {}'.format(step))
         if self.current_image_path and self.current_image_path in self.image_queue:
             current_index = self.image_queue.index(self.current_image_path)
             if current_index == 0:
                 LOGGER.warning('Already operated the first image in the queue')
             elif current_index - step < 0:
-                LOGGER.warning('Out of index: current {}')
+                LOGGER.warning('Out of index: current {}, target {}'.format(
+                    current_index, current_index-step
+                ))
             else:
-                self.current_image_path = self.image_queue[max(0, current_index-1)]
+                self.current_image_path = self.image_queue[max(0, current_index-step)]
                 LOGGER.info('Ready to process {}/{} image {}'.format(
                     current_index+1, len(self.image_queue), self.current_image_path
                 ))
@@ -33,12 +36,17 @@ class MothKeyboardHandler(MothViewerTemplate):
 
     # press DOWN and switch to next image in image queue
     def switch_to_next_image(self, event=None, step=1):
+        LOGGER.info('step = {}'.format(step))
         if self.current_image_path and self.current_image_path in self.image_queue:
             current_index = self.image_queue.index(self.current_image_path)
             if current_index == len(self.image_queue) - 1:
                 LOGGER.warning('Already operated the last image in the queue')
+            elif current_index + step >= len(self.image_queue):
+                LOGGER.warning('Out of index: current {}, target {}'.format(
+                    current_index, current_index+step
+                ))
             else:
-                self.current_image_path = self.image_queue[min(current_index+1, len(self.image_queue)-1)]
+                self.current_image_path = self.image_queue[min(current_index+step, len(self.image_queue)-1)]
                 LOGGER.info('Ready to process {}/{} image {}'.format(
                     current_index+1, len(self.image_queue), self.current_image_path
                 ))
