9614f8d8e46d15d959b306c6daed8f38488df300
afunTW <endlessproof@gmail.com>
2017-09-15 15:25:05 +0800

    move: move converter form view to support

---
+++ src/support/tkconvert.py
@@ -0,0 +1,21 @@
+"""
+Some support function for tkinter
+"""
+import logging
+import tkinter
+
+import numpy as np
+from PIL import Image, ImageTk
+
+LOGGER = logging.getLogger(__name__)
+
+class TkConverter(object):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def ndarray_to_photo(arr):
+        h, w = arr.shape[0], arr.shape[1]
+        byte_photo = Image.frombytes('L', (w, h), arr.astype('b').tostring())
+        tk_photo = ImageTk.PhotoImage(byte_photo)
+        return tk_photo
