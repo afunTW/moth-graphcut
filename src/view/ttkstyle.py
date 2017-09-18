55a034bff93b1f842f9a6401dccf5016efdc3d38
afunTW <endlessproof@gmail.com>
2017-09-18 16:55:08 +0800

    modified: use alt theme and default init_css font family

--- src/view/ttkstyle.py
+++ src/view/ttkstyle.py
@@ -9,15 +9,23 @@ LOGGER = logging.getLogger(__name__)
 class TTKStyle(ttk.Style):
     def __init__(self, style_name, **kwargs):
         super().__init__()
+        self.theme_use('alt')
         self.configure(style_name, **kwargs)
 
 def init_css():
-    TTKStyle('H1.TLabel', font=('Helvetica',32))
-    TTKStyle('H2.TLabel', font=('Helvetica',24))
-    TTKStyle('H3.TLabel', font=('Helvetica',18))
-    TTKStyle('H4.TLabel', font=('Helvetica',16))
-    TTKStyle('H5.TLabel', font=('Helvetica',13))
-    TTKStyle('H6.TLabel', font=('Helvetica',10))
+    TTKStyle('H1.TLabel', font=('',32, 'bold'), background='white')
+    TTKStyle('H2.TLabel', font=('',24, 'bold'), background='white')
+    TTKStyle('H3.TLabel', font=('',18), background='white')
+    TTKStyle('H4.TLabel', font=('',16), background='white')
+    TTKStyle('H5.TLabel', font=('',13), background='white')
+    TTKStyle('H6.TLabel', font=('',10), background='white')
+    TTKStyle('H1.TCheckbutton', font=('',32), background='white')
+    TTKStyle('H2.TCheckbutton', font=('',24), background='white')
+    TTKStyle('H3.TCheckbutton', font=('',18), background='white')
+    TTKStyle('H4.TCheckbutton', font=('',16), background='white')
+    TTKStyle('H5.TCheckbutton', font=('',13), background='white')
+    TTKStyle('H6.TCheckbutton', font=('',10), background='white')
+    TTKStyle('White.Horizontal.TScale', background='white')
 
 if __name__ == '__main__':
     import tkinter
