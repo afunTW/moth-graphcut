544cda095c271f446a1bbe31276cd09e348f899d
afunTW <endlessproof@gmail.com>
2017-09-18 11:55:44 +0800

    add: default setting of tkinter.LabelFrame

--- src/view/tkframe.py
+++ src/view/tkframe.py
@@ -2,11 +2,19 @@
 Predefined frame
 """
 import logging
-from tkinter import Frame
+from tkinter import Frame, LabelFrame
 
 LOGGER = logging.getLogger(__name__)
 
 class TkFrame(Frame):
+    def __init__(self, *args, **kwargs):
+        if 'padx' not in kwargs:
+            kwargs['padx'] = 10
+        if 'pady' not in kwargs:
+            kwargs['pady'] = 10
+        super().__init__(*args, **kwargs)
+
+class TkLabelFrame(LabelFrame):
     def __init__(self, *args, **kwargs):
         if 'padx' not in kwargs:
             kwargs['padx'] = 10
