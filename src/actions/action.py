f185240865a02fa7c3e18fcfd2c3b83de8582edf
afunTW <endlessproof@gmail.com>
2017-09-18 19:01:51 +0800

    add: detector appearance action

--- src/actions/action.py
+++ src/actions/action.py
@@ -18,6 +18,24 @@ class MothActionsTemplate(MothKeyboardHandler):
         self.root.bind(tkconfig.KEY_UP, self.switch_to_previous_image)
         self.root.bind(tkconfig.KEY_DOWN, self.switch_to_next_image)
 
+        # default binding: detector
+        self.checkbtn_manual_detect.configure(command=self._invoke_manual_detect)
+        self.checkbtn_template_detect.configure(command=self._invoke_template_detect)
+
+    # invoke the callback when the manual detection checkbutton changed state
+    def _invoke_manual_detect(self):
+        if self.checkbtn_manual_detect.instate(['selected']):
+            self.val_template_detect.set(False)
+        elif self.checkbtn_manual_detect.instate(['!selected']):
+            pass
+
+    # invoke the callback when the template detection checkbutton changed state
+    def _invoke_template_detect(self):
+        if self.checkbtn_template_detect.instate(['selected']):
+            self.val_manual_detect.set(False)
+        elif self.checkbtn_template_detect.instate(['!selected']):
+            pass
+
 if __name__ == '__main__':
     """testing"""
     logging.basicConfig(
@@ -30,9 +48,11 @@ if __name__ == '__main__':
     from inspect import currentframe, getframeinfo
     from os import path
     _FILE = path.abspath(getframeinfo(currentframe()).filename)
+    TEMPLATE_IMG = path.abspath('../../image/10mm.png')
     SAMPLE = path.abspath('../../image/sample/')
     SAMPLE_IMGS = sorted([i for i in glob(path.join(SAMPLE, '*.jpg'))])
 
     action = MothActionsTemplate()
+    action.input_template(TEMPLATE_IMG)
     action.input_image(*SAMPLE_IMGS)
     action.mainloop()
