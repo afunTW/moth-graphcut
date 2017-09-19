966d1e05f9e6430e6d043d7ade54369325a313a2
afunTW <endlessproof@gmail.com>
2017-09-19 22:23:19 +0800

    bug: add the old detector to test

--- src/actions/action.py
+++ src/actions/action.py
@@ -7,34 +7,62 @@ import sys
 sys.path.append('../..')
 from src import tkconfig
 from src.actions.keyboard import MothKeyboardHandler
+from src.actions.detector import TemplateDetector
+from src.actions.detector_test import ShapeDetector
 from src.support.profiling import func_profiling
 
 LOGGER = logging.getLogger(__name__)
+STATE_DETECTED = 'detected'
 
 class MothActionsTemplate(MothKeyboardHandler):
     def __init__(self):
-        # default binding
         super().__init__()
+        self.panel_image_state = []
+        self.detector = None
+
+        # default binding
         self.root.bind(tkconfig.KEY_UP, self.switch_to_previous_image)
         self.root.bind(tkconfig.KEY_DOWN, self.switch_to_next_image)
 
         # default binding: detector
         self.checkbtn_manual_detect.configure(command=self._invoke_manual_detect)
         self.checkbtn_template_detect.configure(command=self._invoke_template_detect)
+        self._sync_detection()
 
-    # invoke the callback when the manual detection checkbutton changed state
+    # unique the element in list
+    def _unique(self, l):
+        return list(set(l))
+
+    # render manual detector option
     def _invoke_manual_detect(self):
         if self.checkbtn_manual_detect.instate(['selected']):
             self.val_template_detect.set(False)
+            self._sync_detection()
         elif self.checkbtn_manual_detect.instate(['!selected']):
             pass
 
-    # invoke the callback when the template detection checkbutton changed state
+    # render template detector option
     def _invoke_template_detect(self):
         if self.checkbtn_template_detect.instate(['selected']):
             self.val_manual_detect.set(False)
+            self._sync_detection()
         elif self.checkbtn_template_detect.instate(['!selected']):
             pass
+
+    # detect and clear image when detect option was selected
+    def _sync_detection(self):
+        is_manual = self.checkbtn_manual_detect.instate(['selected'])
+        is_template = self.checkbtn_template_detect.instate(['selected'])
+
+        if (is_manual or is_template) and STATE_DETECTED not in self.panel_image_state:
+            if self.image_template is not None:
+                if self.detector is None:
+                    # self.detector = TemplateDetector(self.image_path_template, self.current_image_path)
+                    self.detector = ShapeDetector(self.image_path_template, self.current_image_path)
+                x, y, w, h = self.detector.detect_template()
+                cv2.rectangle(self.image_panel, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+        self.root.after(100, self._sync_detection)
 
 if __name__ == '__main__':
     """testing"""
