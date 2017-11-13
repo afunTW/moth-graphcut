5bd74556b520ddec9b1a43634677f3154ffa1424
afunTW <endlessproof@gmail.com>
2017-11-13 17:46:10 +0800

    remove: old src

--- src_bk/color.py
+++
@@ -1,8 +0,0 @@
-class RGB(object):
-    def __init__(self):
-        super().__init__()
-        self.BLUE = [255,0,0]
-        self.RED = [0,0,255]
-        self.GREEN = [0,255,0]
-        self.BLACK = [0,0,0]
-        self.WHITE = [255,255,255]
